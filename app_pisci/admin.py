fba60a963487abbaf740374efe489fc01531fbc3
Annece2935 <anne-cecile.colleter@live.fr>
2025-09-08 07:09:39 +0200

    add autorisation to admin to bassin, site ans relevetempeoxy

--- app_pisci/admin.py
+++ app_pisci/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Site, Bassin, ReleveTempOxy
 
 # Register your models here.
+admin.site.register(Site)
+admin.site.register(Bassin)
+admin.site.register(ReleveTempOxy)
