d0bfb82ef0a9722ab2cb9b80f7d486ddd125a3f8
Annece2935 <anne-cecile.colleter@live.fr>
2025-10-08 14:51:25 +0200

    feat(admin): configurer l'interface d'administration pour Repas Journalier
    - Ajout de `NourrissageAdmin` pour gérer les repas journaliers dans l'admin Django
    - Configuration des colonnes affichées :
      - code_lot (via propriété)
      - bassin, date_repas, qte, aliment, site_prod, cree_par, created_at
    - Ajout de filtres par `date_repas`, `aliment`, et `site_prod`
    - Recherche possible par `code_lot` et `nom du bassin`
    - Affichage personnalisé du `code_lot` via une méthode dédiée

--- app_pisci/apps/activite_quotidien/admin.py
+++ app_pisci/apps/activite_quotidien/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import ReleveTempOxy
+from .models import ReleveTempOxy, Nourrissage
 
+admin.register(Nourrissage)
 admin.site.register(ReleveTempOxy)
+
+class NourrissageAdmin(admin.ModelAdmin):
+    list_display = (
+        'code_lot', 'bassin',
+        'date_repas', 'qte', 'aliment',
+        'site_prod', 'notes'
+    )
+
+    def code_lot(self, obj):
+        return obj.code_lot
+    code_lot.short_description = "Code lot"
