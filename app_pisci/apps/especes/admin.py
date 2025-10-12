8ef2803013b553f9717b4849185705c6f4087b7d
Annece2935 <anne-cecile.colleter@live.fr>
2025-10-12 10:08:39 +0200

    feat(admin): ajouter configuration pour EspèceAdmin - Affichage des champs : nom_scientifique, nom_commun, temperature_ideale - Tri par défaut sur nom_commun - Filtres par famille - Recherche par nom_scientifique et nom_commun

--- app_pisci/apps/especes/admin.py
+++ app_pisci/apps/especes/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Espece
 
 @admin.register(Espece)
-class EspeeAdmin(admin.ModelAdmin):
+class EspeceAdmin(admin.ModelAdmin):
     list_display = ('nom_commun', 'nom_scientifique', 'est_actif')
     list_filter = ('est_actif',)
     search_fields = ('nom_commun', 'nom_scientifique')
