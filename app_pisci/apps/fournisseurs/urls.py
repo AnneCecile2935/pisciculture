f1b31016badd03ac64910a1df930790165769d67
Annece2935 <anne-cecile.colleter@live.fr>
2025-10-20 10:29:27 +0200

    feat(fournisseurs): migre vers un rendu dynamique avec DataTables
    - Adaptation du template `fournisseur_list.html` pour utiliser DataTables avec chargement AJAX.
    - Ajout de la vue `fournisseur_list_json` pour retourner les données au format JSON.
    - Ajout de l'URL `fournisseur-list-json` pour la récupération des données.
    
    - Réapplication des styles CSS aux boutons via `drawCallback` pour les éléments générés dynamiquement.

--- app_pisci/apps/fournisseurs/urls.py
+++ app_pisci/apps/fournisseurs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import FournisseurCreateView, FournisseurDeleteView, FournisseurListView, FournisseurUpdateView
+from .views import FournisseurCreateView, FournisseurDeleteView, FournisseurListView, FournisseurUpdateView, FournisseurListJsonView
 
 app_name = 'fournisseurs'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('create/', FournisseurCreateView.as_view(), name='fournisseur-create'),
     path("<uuid:pk>/update/", FournisseurUpdateView.as_view(), name="fournisseur-update"),
     path("<uuid:pk>/delete/", FournisseurDeleteView.as_view(), name="fournisseur-delete"),
+    path('list/json/', FournisseurListJsonView.as_view(), name='fournisseur-list-json'),
 ]
 
--- app_pisci/apps/fournisseurs/views.py
+++ app_pisci/apps/fournisseurs/views.py
@@ -1,8 +1,10 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .models import Fournisseur
 from django.urls import reverse_lazy
 from .forms import FournisseurForm
+from django.contrib import messages
+from django.http import JsonResponse
 
 class FournisseurListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Fournisseur
@@ -18,6 +20,7 @@ class FournisseurCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateV
     permission_required = "fournisseurs.add_fournisseur"
 
     def form_valid(self, form):
+        messages.success(self.request, "Le fournisseur a été ajouté avec succès !")
         return super().form_valid(form)
 
 
@@ -28,8 +31,25 @@ class FournisseurUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateV
     success_url = reverse_lazy("fournisseurs:fournisseur-list")
     permission_required = "fournisseurs.change_fournisseur"
 
+    def form_valid(self, form):
+        messages.success(self.request, "Le fournisseur a été mis à jour avec succès !")
+        return super().form_valid(form)
+
 class FournisseurDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Fournisseur
     template_name = "fournisseurs/frs_confirm_delete.html"
     success_url = reverse_lazy("fournisseurs:fournisseur-list")
     permission_required = "fournisseurs.delete_fournisseur"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "Le fournisseur a été supprimé avec succès !")
+        return super().delete(request, *args, **kwargs)
+
+class FournisseurListJsonView(LoginRequiredMixin, PermissionRequiredMixin, View):
+    permission_required = "fournisseurs.view_fournisseur"
+
+    def get(self, request, *args, **kwargs):
+        fournisseurs = list(Fournisseur.objects.values(
+            'id', 'nom', 'type_fournisseur', 'ville', 'contact', 'est_actif'
+        ))
+        return JsonResponse(fournisseurs, safe=False)
